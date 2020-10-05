f8e1a05b5afe9df7f59f93f152b6466657e4bf21
Aman Gupta <amangupta6121998@gmail.com>
2020-10-05 19:09:33 +0530

    Add files via upload

---
+++ 01-Basics of JS/#03  CODING CHALLENGE 1/challenge1.js
@@ -0,0 +1,44 @@
+//----------------CODING CHALLENGE 1----------------------
+
+
+var mheight = prompt('enter marks height');
+
+var mmass= prompt('enter marks mass');
+
+var BMI = mmass/(mheight*mheight);
+console.log('MARKS HEIGHT IS '+mheight);
+console.log('MARKS MASS IS '+mmass);
+console.log('MARKS BMI is '+BMI);
+
+
+var jheight = prompt('ENTER JHONS HEIGHT');
+var jmass = prompt('ENTER JHONS MASS');
+var jBMI = jmass/(jheight*jheight);
+console.log('JHONS HEIGHT IS '+jheight);
+console.log('JHONS MASS IS '+jmass);
+console.log('JHONS BMI IS '+jBMI);
+
+
+var comp = BMI>jBMI;
+console.log('MARKS BMI IS GREATER THAN JHONS???? '+comp);
+
+
+
+//-------SOLVING CODE CHALLENGE 1 BY IF/ELSE STATEMENT--------
+
+if(BMI>jBMI)
+    {
+        
+        
+        console.log('MARK HAS A GREATER BMI AND HIS BMI IS = '+BMI);
+        
+    }
+
+else
+    {
+        
+        console.log('JHONS HAS GREATER BMI AND HIS BMI IS = '+jBMI);
+    }
+
+
+
