fe50f5c894ae27d04468f5b970dbc9a7a82649a8
Aman Gupta <amangupta6121998@gmail.com>
2020-10-05 19:09:58 +0530

    Add files via upload

---
+++ 01-Basics of JS/#04  IF-ELSE Statements/if-else.js
@@ -0,0 +1,55 @@
+/*
+-----------------IF/ELSE STAEMENTS IN JAVASCRIPT--------------
+   
+*/
+
+//---------------SIMPLE IF/ELSE STATEMENT CONCEPT ------------
+
+var username = 'JHON';
+var age = 18;
+
+if(age ===18)
+    {
+        
+        console.log(username + ' Can vote');
+    }
+else
+    {
+        
+        console.log('CAN NOT VOTE AS YOUR AGE IS LESS THAN 18');
+    }
+
+
+// ----------USING BOOLEAN VALUE IN IF/ELSE STATEMENT--------
+
+var prof = 'CODER';
+
+var skill = false;
+
+if(skill)
+    {
+        // if statement is true then this block will execute
+        console.log(prof+' has a SKILL in JS'); 
+    }
+else
+    {
+        // if statement is false then this block will execute
+        console.log(prof+' doesn\'t  have SKILLS in JS !!!');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
