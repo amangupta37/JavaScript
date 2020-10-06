9cbba165ca0ee8a2e115111237414b6254c1ac87
Aman Gupta <amangupta6121998@gmail.com>
2020-10-06 19:42:15 +0530

    Add files via upload

---
+++ 01-Basics of JS/#06  TERNARY OPERATOR AND SWITCH STATEMENT/ternary_switch.js
@@ -0,0 +1,65 @@
+/*
+----------------TERNARY OPERATOR AND SWITCH STATEMENT----------
+
+*/
+
+//---------------------TERNARY OPERATOR------------------------
+
+
+var name = 'SMITH';
+var age = 15;
+
+var drink = age>=18 ? 'CAN DRINK BEAR !!':'CAN DRINK JUICE';
+
+
+console.log(name+' YOU '+drink);
+
+//-----------------SWITCH STATEMENT----------------------------
+
+
+var job='teacher';
+
+switch(job)
+    {
+            case 'teacher':
+            console.log(name+' has teaching job');
+            break;
+            
+            case 'cop':
+            console.log(name+' has police officer job');
+            break;
+            
+            case 'coder':
+            console.log(name+' has developer  job');
+            break;
+            
+            case 'chef':
+            console.log(name+' has cocking job');
+            break;
+            
+        default:
+            console.log(name+' has diff job');
+    }
+
+// ------------BOOLEAN LOGIC AND SWITCH STATEMENT-----------
+
+age =14
+switch(true) // for comparing integer we can use boolean value also to pass the value of variable in switch statement 
+{
+          case age<13:
+            console.log(name+' has teaching job');
+            break;
+            
+            case age>=13&&age<=20:
+            console.log(name+' has police officer job');
+            break;
+            
+            case age>=20&&age<30:
+            console.log(name+' has developer  job');
+            break;
+            
+           
+        default:
+            console.log(name+' has diff job');
+}
+
