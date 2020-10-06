b66737c1658ce7c1a13a95e811cef96c08819e72
Aman Gupta <amangupta6121998@gmail.com>
2020-10-06 19:47:05 +0530

    Update ternary_switch.js

--- 01-Basics of JS/#06  TERNARY OPERATOR AND SWITCH STATEMENT/ternary_switch.js
+++ 01-Basics of JS/#06  TERNARY OPERATOR AND SWITCH STATEMENT/ternary_switch.js
@@ -4,6 +4,12 @@
 */
 
 //---------------------TERNARY OPERATOR------------------------
+/*
+1. The conditional (ternary) operator is the only JavaScript operator that takes three operands: a condition followed by a question mark (?), 
+   then an expression to execute if the condition is truthy followed by a colon (:), and finally the expression to execute if the condition is 
+   falsy. 
+
+*/
 
 
 var name = 'SMITH';
